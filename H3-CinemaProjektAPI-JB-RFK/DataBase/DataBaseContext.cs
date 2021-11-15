4a46cd4577f2bc19bd4ce31f75abf8e1adcf3446
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2021-11-15 10:47:55 +0100

    DatabaseContext: Booking Directors Hall Movie PaymentDetails Profile SeatNumber

---
+++ H3-CinemaProjektAPI-JB-RFK/DataBase/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using H3_CinemaProjektAPI_JB_RFK.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H3_CinemaProjektAPI_JB_RFK.DataBase
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        public DbSet<Booking> Booking { get; set; }
+        public DbSet<Directors> Directors { get; set; }
+        public DbSet<Hall> Hall { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+        public DbSet<Profile> Profile { get; set; }
+        public DbSet<SeatNumber> SeatNumber { get; set; }
+
+    }
+}
