8c8e68f0c31b64bc1c40d0a019fa5a8dc84de139
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2021-11-15 10:07:23 +0100

    Models Hall

---
+++ H3-CinemaProjektAPI-JB-RFK/Model/Hall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H3_CinemaProjektAPI_JB_RFK.Model
+{
+    public class Hall
+    {
+        public int HallId { get; set; }
+        public string HallGreat { get; set; }
+        public string HallSmall { get; set; }
+
+    }
+}
