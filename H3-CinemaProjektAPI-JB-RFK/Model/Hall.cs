78cba8c0450bdca72ca234236b7142fca527c99b
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2021-11-18 10:33:26 +0100

    changed Hall modell

--- H3-CinemaProjektAPI-JB-RFK/Model/Hall.cs
+++ H3-CinemaProjektAPI-JB-RFK/Model/Hall.cs
@@ -9,8 +9,7 @@ namespace H3_CinemaProjektAPI_JB_RFK.Model
     {
         public int HallId { get; set; }
         public int SeatNumberId { get; set; }
-        public bool isGreatHAll { get; set; }
-        public bool isSmallHall { get; set; }
+        public string HAllName { get; set; }
 
         /* EF Relations */
         public ICollection<SeatNumber> SeatNumbers { get; set; }
